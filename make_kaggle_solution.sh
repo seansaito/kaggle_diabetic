4a7cf3e264d13a6dc0846829d7279a191027108a
Sean Saito <saitosean@ymail.com>
2016-11-18 22:39:09 +0800

    Updated requirements

--- make_kaggle_solution.sh
+++ make_kaggle_solution.sh
@@ -17,20 +17,26 @@ python convert.py --crop_size 512 --convert_directory data/test_medium --extensi
 
 # For the training set, make smaller images to train smaller versions of the 
 # convolutional networks.
-python convert.py --crop_size 256 --convert_directory data/train_small --extension tiff --directory data/train_medium
+#python convert.py --crop_size 256 --convert_directory data/train_small --extension tiff --directory data/train_medium
 python convert.py --crop_size 128 --convert_directory data/train_tiny --extension tiff --directory data/train_medium
+
+echo "Converting done"
 
 ########## Train Convolutional Networks ##########
 
 # Train network with 5x5 and 3x3 kernels.
-python train_nn.py --cnf configs/c_128_5x5_32.py
-python train_nn.py --cnf configs/c_256_5x5_32.py --weights_from weights/c_128_5x5_32/weights_final.pkl
-python train_nn.py --cnf configs/c_512_5x5_32.py --weights_from weights/c_256_5x5_32/weights_final.pkl
+#python train_nn.py --cnf configs/c_128_5x5_32.py
+#python train_nn.py --cnf configs/c_256_5x5_32.py --weights_from weights/c_128_5x5_32/weights_final.pkl
+#python train_nn.py --cnf configs/c_512_5x5_32.py --weights_from weights/c_256_5x5_32/weights_final.pkl
+
+echo "Training"
 
 # Train network with 4x4 kernels.
 python train_nn.py --cnf configs/c_128_4x4_32.py
-python train_nn.py --cnf configs/c_256_4x4_32.py --weights_from weights/c_128_4x4_32/weights_final.pkl
-python train_nn.py --cnf configs/c_512_4x4_32.py --weights_from weights/c_256_4x4_32/weights_final.pkl
+#python train_nn.py --cnf configs/c_256_4x4_32.py --weights_from weights/c_128_4x4_32/weights_final.pkl
+#python train_nn.py --cnf configs/c_512_4x4_32.py --weights_from weights/c_256_4x4_32/weights_final.pkl
+
+echo "Training done"
 
 ########## Extract Features ##########
 
@@ -39,17 +45,17 @@ python train_nn.py --cnf configs/c_512_4x4_32.py --weights_from weights/c_256_4x
 # or even fewer or by only extracting the features for a single set of weights.
 
 # Extract features for network with 5x5 and 3x3 kernels.
-BEST_VALID_WEIGHTS="$(ls -t weights/c_512_5x5_32/best/ | head -n 1)"
-python transform.py --cnf configs/c_512_5x5_32.py --train --test --n_iter 50 --weights_from weights/c_512_5x5_32/best/"$BEST_VALID_WEIGHTS"
+#BEST_VALID_WEIGHTS="$(ls -t weights/c_128_4x4_32/best/ | head -n 1)"
+#python transform.py --cnf configs/c_128_4x4_32.py --train --test --n_iter 20 --weights_from weights/c_128_4x4_32/best/"$BEST_VALID_WEIGHTS"
 # by default weights with best validation kappa druing train run are loaded
-python transform.py --cnf configs/c_512_5x5_32.py --train --test --n_iter 50 --skip 50
-python transform.py --cnf configs/c_512_5x5_32.py --train --test --n_iter 50 --skip 100 --weights_from weights/c_512_5x5_32/weights_final.pkl
+#python transform.py --cnf configs/c_512_5x5_32.py --train --test --n_iter 50 --skip 50
+#python transform.py --cnf configs/c_512_5x5_32.py --train --test --n_iter 50 --skip 100 --weights_from weights/c_512_5x5_32/weights_final.pkl
 
 # Extract features for network with 4x4 kernels.
-BEST_VALID_WEIGHTS="$(ls -t weights/c_512_4x4_32/best/ | head -n 1)"
-python transform.py --cnf configs/c_512_4x4_32.py --train --test --n_iter 50 --weights_from weights/c_512_4x4_32/best/"$BEST_VALID_WEIGHTS"
-python transform.py --cnf configs/c_512_4x4_32.py --train --test --n_iter 50 --skip 50
-python transform.py --cnf configs/c_512_4x4_32.py --train --test --n_iter 50 --skip 100 --weights_from weights/c_512_4x4_32/weights_final.pkl
+BEST_VALID_WEIGHTS="$(ls -t weights/c_128_4x4_32/best/ | head -n 1)"
+python transform.py --cnf configs/c_128_4x4_32.py --train --test --n_iter 20 --weights_from weights/c_128_4x4_32/best/"$BEST_VALID_WEIGHTS"
+python transform.py --cnf configs/c_128_4x4_32.py --train --test --n_iter 20 --skip 50
+python transform.py --cnf configs/c_128_4x4_32.py --train --test --n_iter 20 --skip 100 --weights_from weights/c_128_4x4_32/weights_final.pkl
 
 ########## Blend Features ##########
 
